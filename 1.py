a4842adbb6b3d6dfd57b0f17877004686d01428d
Sahar Yadegari <100509610+Saha-r@users.noreply.github.com>
2024-06-14 22:43:34 +0330

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print('Hello World')
+print("It's alright")
+print("I'm 'Sahar'")
+print('I\'m "Sahar"')
+print("""kfmkffiwkrpfl
+      fkkflgefkmegmkeg
+      flklrrlerelrg""")
