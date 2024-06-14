e172e164c1efe5a7bd72a67444cd453256fe9d41
Sahar Yadegari <100509610+Saha-r@users.noreply.github.com>
2024-06-14 22:59:03 +0330

    #new

--- 1.py
+++ 1.py
@@ -6,3 +6,17 @@ print('I\'m "Sahar"')
 print("""kfmkffiwkrpfl
       fkkflgefkmegmkeg
       flklrrlerelrg""")
+s = "Hello World"
+print(s[8])
+
+for x in "banana":
+    print(x)
+print(len("banana"))
+print(len(s))
+print("n" in "banana")
+print("n" not in "banana")
+print("s" in "banana")
+if "a" in "banana":
+    print("a is in banana")
+if "s" not in "banana":
+    print("s is not in banana")
